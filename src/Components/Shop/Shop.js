5b470125beebed6024460e34d720f004939a0854
Jahidul-Bin-Rafiq <jahidulrafiq@gmail.com>
2020-08-23 19:58:38 +0600

    update

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../fakeData';
+import fakeData from '../../fakeData';
+const Shop = () => {
+   console.log(fakeData)
+    return (
+        <div>
+            <h1>This shop page</h1>
+        </div>
+    );
+};
+
+export default Shop;
